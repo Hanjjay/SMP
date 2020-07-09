cba794500e8082712fbf9728ffb7e4460c79ef7a
Hanjay <3354945@gmail.com>
2020-07-09 12:49:22 +0900

    Hanjjay
    back up SMC

--- django_web-master/blog/models.py
+++ django_web-master/blog/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class PSD(models.Model):
-    PSD_value = models.FloatField(blank=True, default=0)
-    Freq_value = models.FloatField(blank=True, default=0)
-
-    PSD_svg = models.FloatField(blank=True, default=0)
-    Freq_svg = models.FloatField(blank=True, default=0)
-
-    sc_tag = models.CharField(blank=True, max_length=200)
--- django_web-master/blog/views.py
+++ django_web-master/blog/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-from .models import PSD
 
 # Create your views here.
-
 def index(request):
     return render(request, 'page/home.html')
 
-def method(request):
-    return render(request, 'page/method.html')
+def analysis(request):
+    return render(request, 'page/analysis.html')
 
-def conclusion(request):
-    return render(request, 'page/conclusion.html')
-
-def result(request):
-    return render(request, 'page/result.html')
+def map(request):
+    return render(request, 'page/map.html')
